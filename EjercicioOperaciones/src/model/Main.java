73e6861a4e1913b1521cc9c554f5ac439c152b66
Camilo Bonilla <camilobonillah@uptc.edu.co>
2017-08-09 18:42:19 -0500

    deshaciendo

--- EjercicioOperaciones/src/model/Main.java
+++ EjercicioOperaciones/src/model/Main.java
@@ -1,14 +1,13 @@
 package model;
 
 import logic.Sum;
-import logic.resta;
+
 
 public class Main {
 	public static void main(String[] args) {
 		Sum sum=new Sum(100,345); 
 
-		resta r = new resta();
-		System.out.println("la resta es: " + r.restNumbers(45,21));
+		
 	}
 
 }
